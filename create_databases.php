c029fda5f8804f709439af363fb7e9ea875ba372
vinuthi2091 <vinuthithedara@gmail.com>
2026-04-16 14:56:19 +0530

    create_databases.php
    create tables: users, admin, affected_people, volunteers tables.

---
+++ create_databases.php
@@ -0,0 +1,84 @@
+<?php
+
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "disaster_management_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//Create database
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+$conn->query($sql);
+
+// Select database
+$conn->select_db($dbname);
+
+// Create sers table
+$sql = "CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    user_role ENUM('admin', 'affected', 'volunteer') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+$conn->query($sql);
+
+//Create Admin table
+$sql = "CREATE TABLE IF NOT EXISTS admin (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    contact_no VARCHAR(15),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)";
+$conn->query($sql);
+
+//Create Affected people table
+$sql = "CREATE TABLE IF NOT EXISTS affected_people (
+    affected_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE,
+    full_name VARCHAR(100) NOT NULL,
+    nic VARCHAR(20),
+    contact_no VARCHAR(15),
+    address TEXT,
+    district VARCHAR(50),
+    no_of_family_members INT,
+    urgent_level ENUM('low', 'medium', 'high'),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)";
+$conn->query($sql);
+
+//Create Volunteers table
+$sql = "CREATE TABLE IF NOT EXISTS volunteers (
+    volunteer_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE,
+    full_name VARCHAR(100) NOT NULL,
+    nic VARCHAR(20),
+    contact_no VARCHAR(15),
+    address TEXT,
+    district VARCHAR(50),
+    availability_status ENUM('available', 'busy') DEFAULT 'available',
+    organization_name VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)";
+$conn->query($sql);
+
+echo "All tables created successfully!";
+
+$conn->close();
+?>
