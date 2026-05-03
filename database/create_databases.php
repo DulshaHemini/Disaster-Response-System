443480f108cd58c9c7ce8f6da6b4f0a65aaf6bc4
DasinduDilvan <dilwanwije@gmail.com>
2026-05-03 17:10:40 +0530

    DB changed and resouece, assignment tables created

--- database/create_databases.php
+++ database/create_databases.php
@@ -87,19 +87,63 @@ $sql = "CREATE TABLE IF NOT EXISTS Request(
     req_id INT PRIMARY KEY AUTO_INCREMENT,
     req_name VARCHAR(255) NOT NULL,
     req_type VARCHAR(50) NOT NULL,
+    resource_type ENUM('Medicals', 'Foods', 'Shelters', 'Cloths', 'Money') NOT NULL,
+    resource_count INT,
+    no_of_affected_people INT,
     description VARCHAR(256),
     contact_number VARCHAR(20) NOT NULL,
     priority_level ENUM('low', 'medium', 'high'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50) DEFAULT 'Pending',
+    is_instant TINYINT(1) DEFAULT 0,
     loc_id INT,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-    ON UPDATE CASCADE,
-    FOREIGN KEY (loc_id) REFERENCES Location(loc_id)
+    user_id INT DE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE SET NULL,
+    FOREIGN KEY (loc_id) REFERENCES Location(loc_id) ON UPDATE CASCADE
+)";
+$conn->query($sql);
+
+
+$sql = "CREATE TABLE IF NOT EXISTS resourc(
+    resource_id INT PRIMARY KEY AUTO_INCREMENT,
+    volunteer_id INT,
+    resource_name VARCHAR (100),
+    resource_type ENUM('Medicals', 'Foods', 'Shelters', 'Cloths', 'Money') NOT NULL,
+    resource_count INT,
+    description TEXT,
+    FOREIGN KEY (volunteer_id) REFERENCES volunteer (user_id)
     ON DELETE CASCADE
 )";
 $conn->query($sql);
+
+
+$sql = "CREATE TABLE IF NOT EXISTS assignment(
+    assignment_id INT PRIMARY KEY AUTO_INCREMENT,
+    assigned_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    req_id INT NOT NULL,
+    resource_id INT,
+    volunteer_id INT,
+    description TEXT,
+    status ENUM('Assigned', 'Allocated', 'Received') NOT NULL,
+    FOREIGN KEY (req_id) REFERENCES Request(req_id) ON DELETE CASCADE,
+    FOREIGN KEY (resource_id) REFERENCES resourc(resource_id),
+    FOREIGN KEY (volunteer_id) REFERENCES volunteer(user_id)
+)";
+$conn->query($sql);
+
+
+$sql = "CREATE TABLE IF NOT EXISTS money_allocation(
+    allocation_id INT PRIMARY KEY AUTO_INCREMENT,
+    admin_id INT NOT NULL,
+    req_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    note TEXT,
+    allocated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (admin_id) REFERENCES admin(user_id),
+    FOREIGN KEY (req_id) REFERENCES Request(req_id) ON DELETE CASCADE
+)";
+$conn->query($sql);
+
 
 echo "All tables created successfully!";
 
