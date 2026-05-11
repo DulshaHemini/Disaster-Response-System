bc673ec45e084b912008a5eeb6882f955af1e2af
Dilmi2003625 <dhettiarachchige111@gmail.com>
2026-05-11 18:18:58 +0530

    implemented loggedRequest module: request insertion and location integration

--- app/models/loggedRequest_.php
+++ app/models/loggedRequest_.php
@@ -1,9 +1,10 @@
+<?php
 function insertData(){
     require_once __DIR__ . "../../config/config.php";
 
     $username = $_POST['username'];
-    $Request_name = $_POST['request_name'];
-    $Request_type = $_POST['req_type'];
+    $request_name = $_POST['request_name'];
+    $request_type = $_POST['req_type'];
     $description = $_POST['description'];
     $affected_people = $_POST['affected_people'];
     $resource_type = $_POST['resource_type'];
@@ -17,6 +18,51 @@ function insertData(){
     $district = $_POST['district'];
     $location = $_POST['location'];
 
+    $sql = "INSERT INTO logged_Request(
+        username,
+        request_type,
+        description,
+        affected_people,
+        resource_type,
+        resource_count,
+        contact_number,
+        email,
+        city,
+        street,
+        home_no,
+        priority,
+        district,
+        location
 
+        )VALUES ('$username',
+        '$request_name',
+        '$request_type',
+        '$description',
+        '$affected_people',
+        '$resource_type',
+        '$resource_count',
+        '$contact_number',
+        '$email',
+        '$city',
+        '$street',
+        '$home_no',
+        '$priority',
+        '$district',
+        '$location')
+    )";
 
+  return mysqli_query($conn, $sql);
+    
+  // i want to update location table with the new location data
+  $sql_location = "INSERT INTO Location(
+    latitude,
+    longitude,
+    district,
+    city,
+    street,
+    home_no) VALUES ('$latitude', '$longitude', '$district', '$city', '$street', '$home_no')";
+  return mysqli_query($conn, $sql_location);
+  
 }
+
+?>
