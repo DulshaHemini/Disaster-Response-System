05de9f74bb2d4028f578123ce8a37d97af818432
Dilmi2003625 <dhettiarachchige111@gmail.com>
2026-05-13 10:49:54 +0530

    Modifiy delete_test_data.php

--- database/delete_test_data.php
+++ database/delete_test_data.php
@@ -5,8 +5,12 @@ require_once "../config/config.php";
 // Disable foreign key checks first
 $conn->query("SET FOREIGN_KEY_CHECKS = 0");
 
-// TRUNCATE all tables
-$conn->query("TRUNCATE TABLE Request");
+// TRUNCATE all tables (correct order to avoid foreign key issues)
+$conn->query("TRUNCATE TABLE assignment");
+$conn->query("TRUNCATE TABLE Instant_Request");
+$conn->query("TRUNCATE TABLE Logged_Request");
+$conn->query("TRUNCATE TABLE requests");
+$conn->query("TRUNCATE TABLE resource");
 $conn->query("TRUNCATE TABLE Location");
 $conn->query("TRUNCATE TABLE volunteer");
 $conn->query("TRUNCATE TABLE affected_people");
