2f9ae5ea95881f5077b8e30ec51092a7e2a853e1
Sendiya <t.v.k.sendiya@gmail.com>
2026-05-18 17:37:32 +0530

    Fix database connection initialization in insert_test_data.php

--- database/insert_test_data.php
+++ database/insert_test_data.php
@@ -6,8 +6,8 @@ $password = "";
 $dbname = "drcs";
 $port = 3307;
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, 3308);
+// Create connectiony
+$conn = new mysqli($servername, $username, $password, "", 3308);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
